df7627ef61235096f2373dd27b76c4d04a75bf59
SibWaf <dya-tel@yandex.ru>
2016-04-14 18:04:34 +0300

    Add java.nio.file.Path TypeAdapter for Gson

---
+++ src/main/java/ru/dyatel/karaka/util/GsonPathAdapter.java
@@ -0,0 +1,23 @@
+package ru.dyatel.karaka.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GsonPathAdapter extends TypeAdapter<Path> {
+
+	@Override
+	public void write(JsonWriter out, Path value) throws IOException {
+		out.value(value.toString());
+	}
+
+	@Override
+	public Path read(JsonReader in) throws IOException {
+		return Paths.get(in.nextString());
+	}
+
+}
