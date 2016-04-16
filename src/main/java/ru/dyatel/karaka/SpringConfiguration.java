cafcd048d7dac01dcddbdfe3559579f4e3e5c6c3
SibWaf <dya-tel@yandex.ru>
2016-04-16 17:59:32 +0300

    Connect Karaka to a database

--- src/main/java/ru/dyatel/karaka/SpringConfiguration.java
+++ src/main/java/ru/dyatel/karaka/SpringConfiguration.java
@@ -1,12 +1,38 @@
 package ru.dyatel.karaka;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.dyatel.karaka.config.DbConnectionConfig;
+import ru.dyatel.karaka.config.KarakaConfigurationManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"ru.dyatel.karaka"})
 public class SpringConfiguration {
+
+	@Bean
+	public DataSource dataSource(KarakaConfigurationManager configManager) {
+		DbConnectionConfig config = configManager.getConfig().getDbConfig();
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName(config.getDriver());
+		dataSource.setUrl(config.getUrl());
+		dataSource.setUsername(config.getUsername());
+		dataSource.setPassword(config.getPassword());
+
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
 
 }
