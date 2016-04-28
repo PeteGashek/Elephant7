21154086968c1732ba5fbfcbf88ddc0561fdaf59
SibWaf <dya-tel@yandex.ru>
2016-04-29 00:06:46 +0300

    Add Post POJO

---
+++ Core/src/main/java/ru/dyatel/karaka/threads/Post.java
@@ -0,0 +1,78 @@
+package ru.dyatel.karaka.threads;
+
+public class Post {
+
+	private Long postId = 0L;
+	private Long threadId = 0L;
+
+	private PostType type = PostType.NORMAL;
+
+	private String name = "";
+	private String message = null;
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public Long getThreadId() {
+		return threadId;
+	}
+
+	public void setThreadId(Long threadId) {
+		this.threadId = threadId;
+	}
+
+	public PostType getType() {
+		return type;
+	}
+
+	public void setType(PostType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Post post = (Post) o;
+
+		if (!postId.equals(post.postId)) return false;
+		if (!threadId.equals(post.threadId)) return false;
+		if (type != post.type) return false;
+		if (!name.equals(post.name)) return false;
+		return message != null ? message.equals(post.message) : post.message == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = postId.hashCode();
+		result = 31 * result + threadId.hashCode();
+		result = 31 * result + type.hashCode();
+		result = 31 * result + name.hashCode();
+		result = 31 * result + (message != null ? message.hashCode() : 0);
+		return result;
+	}
+
+}
