daf91cdb31104084bec3ccabffec67996f516c76
SibWaf <dya-tel@yandex.ru>
2016-04-29 22:32:48 +0300

    Add constants for column names in post tables

---
+++ Core/src/main/java/ru/dyatel/karaka/threads/PostTable.java
@@ -0,0 +1,15 @@
+package ru.dyatel.karaka.threads;
+
+public class PostTable {
+
+	private PostTable() {
+	}
+
+	public static final String POST_ID_COLUMN = "post_id";
+	public static final String THREAD_ID_COLUMN = "thread_id";
+
+	public static final String TYPE_COLUMN = "type";
+	public static final String NAME_COLUMN = "name";
+	public static final String MESSAGE_COLUMN = "message";
+
+}
