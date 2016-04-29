75cb5085f19c286e2e8d4922657a937cda9c619c
SibWaf <dya-tel@yandex.ru>
2016-04-29 22:42:02 +0300

    Add Api request for message posting

--- Core/src/main/java/ru/dyatel/karaka/ApiController.java
+++ Core/src/main/java/ru/dyatel/karaka/ApiController.java
@@ -1,7 +1,15 @@
 package ru.dyatel.karaka;
 
+import org.apache.commons.logging.impl.LogFactoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ru.dyatel.karaka.boards.BoardConfiguration;
+import ru.dyatel.karaka.threads.Post;
+import ru.dyatel.karaka.threads.PostDao;
 import ru.dyatel.karaka.util.ResponseCode;
 
 @RestController
@@ -24,9 +32,32 @@ public class ApiController {
 
 	}
 
+	@Autowired
+	private BoardConfiguration boardConfig;
+
+	@Autowired
+	private PostDao postDb;
+
 	@RequestMapping("/test")
 	public String test() {
 		return "Hello, World!";
+	}
+
+	@RequestMapping(value = "/{boardName}/{threadId}/post", method = RequestMethod.POST)
+	public ApiResponse post(@PathVariable String boardName, Post post) {
+		if (!boardConfig.getBoards().containsKey(boardName))
+			return new ApiResponse(ResponseCode.NO_SUCH_BOARD, "Board \"" + boardName + "\" doesn't exist");
+		if (StringUtils.isEmpty(post.getMessage()))
+			return new ApiResponse(ResponseCode.EMPTY_MESSAGE, "Message is empty");
+
+		try {
+			postDb.post(boardName, post);
+		} catch (Exception e) {
+			LogFactoryImpl.getLog(this.getClass()).error("Got exception while handling post request", e);
+			return new ApiResponse(ResponseCode.INTERNAL_ERROR, "Failed to post a message, try later");
+		}
+
+		return new ApiResponse(ResponseCode.OK);
 	}
 
 }
--- Core/src/main/java/ru/dyatel/karaka/util/ResponseCode.java
+++ Core/src/main/java/ru/dyatel/karaka/util/ResponseCode.java
@@ -2,7 +2,7 @@ package ru.dyatel.karaka.util;
 
 public enum ResponseCode {
 
-	OK(0);
+	OK(0), INTERNAL_ERROR(1), NO_SUCH_BOARD(2), EMPTY_MESSAGE(3);
 
 	private int code;
 
