c4b39bebc5d7bf24448e54b26b0eaeac3bea52a8
SibWaf <dya-tel@yandex.ru>
2016-05-05 21:30:11 +0300

    Add thread creation

--- Core/src/main/java/ru/dyatel/karaka/ApiController.java
+++ Core/src/main/java/ru/dyatel/karaka/ApiController.java
@@ -43,6 +43,23 @@ public class ApiController {
 		return "Hello, World!";
 	}
 
+	@RequestMapping(value = "/{boardName}/post", method = RequestMethod.POST)
+	public ApiResponse createThread(@PathVariable String boardName, Post post) {
+		if (!boardConfig.getBoards().containsKey(boardName))
+			return new ApiResponse(ResponseCode.NO_SUCH_BOARD, "Board \"" + boardName + "\" doesn't exist");
+		if (StringUtils.isEmpty(post.getMessage()))
+			return new ApiResponse(ResponseCode.EMPTY_MESSAGE, "Message is empty");
+
+		try {
+			postDb.createThread(boardName, post);
+		} catch (Exception e) {
+			LogFactoryImpl.getLog(this.getClass()).error("Got exception while handling new thread request", e);
+			return new ApiResponse(ResponseCode.INTERNAL_ERROR, "Failed to create a thread, try later");
+		}
+
+		return new ApiResponse(ResponseCode.OK);
+	}
+
 	@RequestMapping(value = "/{boardName}/{threadId}/post", method = RequestMethod.POST)
 	public ApiResponse post(@PathVariable String boardName, Post post) {
 		if (!boardConfig.getBoards().containsKey(boardName))
--- Core/src/main/java/ru/dyatel/karaka/threads/PostDao.java
+++ Core/src/main/java/ru/dyatel/karaka/threads/PostDao.java
@@ -2,6 +2,8 @@ package ru.dyatel.karaka.threads;
 
 public interface PostDao {
 
+	void createThread(String boardName, Post post);
+
 	void post(String boardName, Post post);
 
 }
