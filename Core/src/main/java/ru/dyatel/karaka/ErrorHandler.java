87bb121426405131ed68833912e703c79c9dfc75
SibWaf <dya-tel@yandex.ru>
2016-05-14 20:11:38 +0300

    Handle validation errors in ErrorHandler

--- Core/src/main/java/ru/dyatel/karaka/ErrorHandler.java
+++ Core/src/main/java/ru/dyatel/karaka/ErrorHandler.java
@@ -3,6 +3,8 @@ package ru.dyatel.karaka;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.impl.LogFactoryImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +21,17 @@ public class ErrorHandler {
 	public ApiResponse fallback(Exception e) {
 		log.error("Caught an unhandled exception", e);
 		return ApiResponse.INTERNAL_ERROR;
+	}
+
+	@ExceptionHandler(BindException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ApiResponse bindingFailure(BindException e) {
+		for (ObjectError error : e.getAllErrors()) {
+			ApiResponse response = ApiResponse.getByMessage(error.getDefaultMessage());
+			if (response != null) return response;
+		}
+		throw new RuntimeException(e);
 	}
 
 }
