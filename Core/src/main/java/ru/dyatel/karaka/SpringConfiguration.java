b807f7aaea16eb76a99d2270ddcd1bb8e6c143f2
SibWaf <dya-tel@yandex.ru>
2016-08-03 19:38:01 +0300

    Add NamedParameterJdbcTemplate to beans

--- Core/src/main/java/ru/dyatel/karaka/SpringConfiguration.java
+++ Core/src/main/java/ru/dyatel/karaka/SpringConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.dyatel.karaka.config.DbConnectionConfig;
 import ru.dyatel.karaka.config.KarakaConfigurationManager;
@@ -37,6 +38,11 @@ public class SpringConfiguration {
 	@Bean
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+		return new NamedParameterJdbcTemplate(dataSource);
 	}
 
 }
