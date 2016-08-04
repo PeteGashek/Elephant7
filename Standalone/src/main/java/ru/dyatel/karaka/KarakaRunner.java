e1baba66e0ed893a72f2d1bc9790387b84dae606
SibWaf <dya-tel@yandex.ru>
2016-08-04 17:32:32 +0300

    Fix Thymeleaf configuration Spring Boot was interfering with it's auto configuration

--- Standalone/src/main/java/ru/dyatel/karaka/KarakaRunner.java
+++ Standalone/src/main/java/ru/dyatel/karaka/KarakaRunner.java
@@ -2,9 +2,10 @@ package ru.dyatel.karaka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {ThymeleafAutoConfiguration.class})
 @Import(SpringConfiguration.class)
 public class KarakaRunner {
 
