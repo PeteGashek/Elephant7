7c14f22bb94d0ca0606caf762d56a6a8a873d0f0
SibWaf <dya-tel@yandex.ru>
2016-08-07 20:42:32 +0300

    Autoconfigure Thymeleaf by Spring Boot

--- Core/src/main/java/ru/dyatel/karaka/WebConfiguration.java
+++ Core/src/main/java/ru/dyatel/karaka/WebConfiguration.java
@@ -1,20 +1,12 @@
 package ru.dyatel.karaka;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.util.List;
 
@@ -24,32 +16,6 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	private GsonHttpMessageConverter gsonHttpMessageConverter;
-
-	@Bean
-	public ViewResolver viewResolver(TemplateEngine templateEngine) {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine);
-		viewResolver.setCharacterEncoding("UTF-8");
-		return viewResolver;
-	}
-
-	@Bean
-	public TemplateEngine templateEngine(ITemplateResolver templateResolver) {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setEnableSpringELCompiler(true);
-		templateEngine.setTemplateResolver(templateResolver);
-		return templateEngine;
-	}
-
-	@Bean
-	public ITemplateResolver templateResolver() {
-		FileTemplateResolver templateResolver = new FileTemplateResolver();
-		templateResolver.setPrefix("resources/");
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setCacheable(false);
-		return templateResolver;
-	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- Standalone/src/main/java/ru/dyatel/karaka/KarakaRunner.java
+++ Standalone/src/main/java/ru/dyatel/karaka/KarakaRunner.java
@@ -2,12 +2,11 @@ package ru.dyatel.karaka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
-@EnableAutoConfiguration(exclude = {ThymeleafAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @PropertySource("file:resources/application.properties")
 @Import(SpringConfiguration.class)
 public class KarakaRunner {
