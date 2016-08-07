104ca8b8ea9da37625340e4b0504a3ab9db05fad
SibWaf <dya-tel@yandex.ru>
2016-08-07 19:59:50 +0300

    Add index page

--- Core/src/main/java/ru/dyatel/karaka/controllers/MainController.java
+++ Core/src/main/java/ru/dyatel/karaka/controllers/MainController.java
@@ -1,11 +1,23 @@
 package ru.dyatel.karaka.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.dyatel.karaka.PageNotFoundException;
+import ru.dyatel.karaka.boards.BoardConfiguration;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	private BoardConfiguration boardConfig;
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("boards", boardConfig.getBoards().keySet());
+		return "index";
+	}
 
 	@RequestMapping("/**")
 	public void noMapping() {
