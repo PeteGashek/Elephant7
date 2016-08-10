828c39335cb220427f50a0928757a2a1297451da
SibWaf <dya-tel@yandex.ru>
2016-08-11 01:34:03 +0300

    Add thread updating with JS

--- Core/src/main/java/ru/dyatel/karaka/SpringConfiguration.java
+++ Core/src/main/java/ru/dyatel/karaka/SpringConfiguration.java
@@ -1,6 +1,8 @@
 package ru.dyatel.karaka;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +15,9 @@ public class SpringConfiguration {
 
 	@Bean
 	public Gson gson() {
-		return new Gson();
+		return new GsonBuilder()
+				.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+				.create();
 	}
 
 	@Bean
--- Core/src/main/resources/js/karaka.js
+++ Core/src/main/resources/js/karaka.js
@@ -1,5 +1,42 @@
 function updateMessages() {
-    console.log("Updating messages.");
+    var request = $.ajax({
+        url: "/api" + window.location.pathname,
+        method: "get"
+    });
+    request.done(function(response) {
+        var postList = $("#post_list");
+        postList.empty();
+        var posts = response.message;
+        for (var i = 0; i < posts.length; i++) {
+            var post = $("<div></div>").addClass("post").attr("id", posts[i].post_id);
+
+            var postHeader = $("<div></div>").addClass("post_header");
+            postHeader.append($("<a></a>").addClass("post_id")
+                .attr("href", "#" + posts[i].post_id)
+                .text("#" + posts[i].post_id)).append(" | ");
+            postHeader.append($("<span></span>").addClass("post_number")
+                .text(i + 1)).append(" | ");
+            postHeader.append($("<span></span>").addClass("post_timestamp")
+                .text(dateFormat(new Date(posts[i].timestamp), "dd.mm.yyyy, HH:MM:ss"))).append(" | ");
+            var posterName = $("<span></span>").text(posts[i].name);
+            switch(posts[i].type) {
+                case "OP":
+                    posterName.addClass("post_op");
+                    break;
+                case "SAGE":
+                    posterName.addClass("post_sage");
+                    break;
+                default:
+                    posterName.addClass("post_normal");
+            }
+            postHeader.append(posterName);
+            post.append(postHeader);
+
+            post.append($("<div></div>").addClass("post_content").text(posts[i].message));
+
+            postList.append(post);
+        }
+    });
 }
 
 $(function() {
@@ -9,7 +46,7 @@ $(function() {
     });
     $("#post_send").click(function() {
         if ($("#new_thread").length) return true;
-        
+
         var form = $("#post_form");
         var request = $.ajax({
             url: "/api" + form.attr("action"),
