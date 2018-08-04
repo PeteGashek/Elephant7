c1028375f21c85c611103cc18428d3ce8839c127
ghost <>
2018-08-04 12:24:21 +0300

    *- removed unused js func

--- src/main/resources/static/js/elephant7.js
+++ src/main/resources/static/js/elephant7.js
@@ -31,42 +31,6 @@ function setAutoupdateNotifierText(text) {
     $("#thread_autoupdate_notifier").text(text);
 }
 
-function constructPost(json, id) {
-    var post = $("<div></div>").addClass("post").attr("id", json.post_id);
-
-    var postHeader = $("<div></div>").addClass("post_header");
-    postHeader.append($("<a></a>").addClass("post_id")
-        .attr("href", "#" + json.post_id)
-        .text("#" + json.post_id)).append(" | ");
-    postHeader.append($("<span></span>").addClass("post_number")
-        .text(id)).append(" | ");
-    postHeader.append($("<span></span>").addClass("post_timestamp")
-        .text(new Date(json.timestamp).format("dd.mm.yyyy, HH:MM:ss"))).append(" | ");
-    var posterName = $("<span></span>").text(json.name);
-    switch(json.type) {
-        case "OP":
-            posterName.addClass("post_op");
-            break;
-        case "SAGE":
-            posterName.addClass("post_sage");
-            break;
-        default:
-            posterName.addClass("post_normal");
-    }
-    postHeader.append(posterName);
-    post.append(postHeader);
-
-    var postContent = $("<div></div>").addClass("post_content");
-    var lines = json.message.split("\n");
-    for (var i = 0; i < lines.length; i++) {
-        postContent.append(document.createTextNode(lines[i]));
-        if (i < lines.length - 1) postContent.append($("<br/>"));
-    }
-    post.append(postContent);
-
-    return post;
-}
-
 function updateMessages() {
     if (autoupdateEnabled) {
         stopAutoupdateTimer();
